aed5fb5d09d389ca836a0e76ac10da348acd9d0f
ShravaniSamal <128453824+ShravaniSamal@users.noreply.github.com>
2025-02-23 15:48:52 +0530

    Create lemp.sh

---
+++ lemp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum update -y
+sudo dnf update -y
+sudo yum install nginx -y
+sudo yum install php -y
+sudo dnf install mariadb105-server -y
+sudo service nginx restart
+sudo service php-fpm restart
+sudo service mariadb restart
+cd /var/www/html
+sudo touch index.html my.php
+echo "apache is working" | sudo tee index.html
+echo "<?php phpinfo(); ?>" | sudo tee my.php
